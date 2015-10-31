87f9cffb19f38c9b7ea64bdec24646fdc5799ea3
Romeo Calota <kicsyromy@gmail.com>
2015-10-31 19:56:16 +0200

    * Add pkconfig generation

--- cmake/ProjectConfig.cmake
+++ cmake/ProjectConfig.cmake
@@ -2,10 +2,14 @@ find_package(PkgConfig)
 pkg_search_module(GOBJECT REQUIRED gobject-2.0>=2.32.4)
 pkg_search_module(GLIB2   REQUIRED glib-2.0>=2.32.4)
 
+set(PKG_CONFIG_REQUIRES "${PKG_CONFIG_REQUIRES} gobject-2.0")
+set(PKG_CONFIG_REQUIRES "${PKG_CONFIG_REQUIRES} glib-2.0")
+
 if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
     pkg_search_module(LIBVLC  REQUIRED libvlc>=2.0.8)
 
     set(LIBMO "${MediaOrganizer_BINARY_DIR}/libmo.so")
+    set(PKG_CONFIG_REQUIRES "${PKG_CONFIG_REQUIRES} libvlc")
 endif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 
 if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
